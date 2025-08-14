31752965309b8e49ac3312a24b9f7820c3b466f8
marcos-j-lemes <marcos.jlf@aluno.ifsc.edu.br>
2025-08-14 18:11:19 -0300

    docs: initial teste

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+
+    public static void hello(int x){
+
+        for(int i = 0; i < x; i++){
+            System.out.println("Hello, World");
+        }
+    }
+
+    public  static void main(String[] args){
+
+        hello(10);
+    }
+}
